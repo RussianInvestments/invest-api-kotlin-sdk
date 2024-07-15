5c3c005c6c9f244ca468ff2ae1099d117d91f375
s.nemets <s.nemets@tinkoff.ru>
2024-07-15 10:39:40 +0300

    * update defineClassLoader function

--- kotlin-sdk-code-generator-plugin/src/main/kotlin/ru/tinvest/piapi/generator/task/CodeGeneratorTask.kt
+++ kotlin-sdk-code-generator-plugin/src/main/kotlin/ru/tinvest/piapi/generator/task/CodeGeneratorTask.kt
@@ -6,8 +6,8 @@ import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import ru.tinvest.piapi.generator.generateFileSpecs
 import java.io.File
+import java.net.URL
 import java.net.URLClassLoader
-import java.util.jar.JarFile
 
 
 abstract class CodeGeneratorTask : DefaultTask() {
@@ -22,7 +22,7 @@ abstract class CodeGeneratorTask : DefaultTask() {
 
     @TaskAction
     fun execute() {
-        defineClassLoader(packageName).use { classLoader ->
+        defineClassLoader().use { classLoader ->
             val fileSpecs = generateFileSpecs(packageName, outputPackage, classLoader)
             val outputDir = outputPath
             if (!outputDir.exists()) {
@@ -35,23 +35,13 @@ abstract class CodeGeneratorTask : DefaultTask() {
         }
     }
 
-    private fun defineClassLoader(packageName: String): URLClassLoader {
+    private fun defineClassLoader(): URLClassLoader {
         val fileList = project.parent?.childProjects?.flatMap { it.value.tasks.getByName("jar").outputs.files.files }
             ?.filter { it.exists() }
             ?: emptyList()
-        val classLoader =
-            URLClassLoader.newInstance(fileList.map { it.toURI().toURL() }.toTypedArray(), this.javaClass.classLoader)
-        fileList.forEach {
-            JarFile(it).use { jarFile ->
-                for (jarEntry in jarFile.entries()) {
-                    if (jarEntry.name.startsWith(packageName.replace(".", "/")) && jarEntry.name.endsWith(".class")) {
-                        var className: String = jarEntry.name.substring(0, jarEntry.name.length - 6) // remove ".class"
-                        className = className.replace('/', '.')
-                        classLoader.loadClass(className)
-                    }
-                }
-            }
-        }
-        return classLoader
+        return URLClassLoader.newInstance(
+            fileList.map { it.toURI().toURL() }.toTypedArray<URL?>(),
+            javaClass.classLoader
+        )
     }
 }
