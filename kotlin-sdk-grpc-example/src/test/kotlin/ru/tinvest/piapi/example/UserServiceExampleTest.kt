e6388366f0cb3d27945cf355e72806aed6ce2f8f
s.nemets <s.nemets@tinkoff.ru>
2024-10-01 16:50:29 +0300

    update examples to have source code

--- kotlin-sdk-grpc-example/src/test/kotlin/ru/tinvest/piapi/example/UserServiceExampleTest.kt
+++ kotlin-sdk-grpc-example/src/test/kotlin/ru/tinvest/piapi/example/UserServiceExampleTest.kt
@@ -6,74 +6,39 @@ import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import ru.tinkoff.piapi.contract.v1.GetAccountsRequest
-import ru.tinvest.piapi.core.InvestApi
 
 class UserServiceExampleTest {
-    companion object {
-        private val logger: Logger = LoggerFactory.getLogger(UserServiceExampleTest::class.java)
-    }
+    private val syncExample = UserServiceSyncExample()
+    private val asyncExample = UserServiceAsyncExample()
 
     @Test
     @DisplayName("Обращение к user service c помощью асинхронного метода")
     fun testUserInfo() {
-        //создаем channel со свойствами по умолчанию
-        val channel = InvestApi.defaultChannel(
-            token = System.getProperty("token"),
-            target = System.getProperty("target")
-        )
-        //создаем объект API
-        val investApi = InvestApi.createApi(channel)
-        val usersService = investApi.usersServiceAsync
         Assertions.assertDoesNotThrow {
             // запрашиваем и выводим все аккаунты в консоль
             runBlocking {
-                val accounts = async { usersService.getAccounts(GetAccountsRequest.getDefaultInstance()) }
-                accounts.await().accountsList.forEach { logger.info("$it") }
+                async {
+                    asyncExample.exampleUserInfo()
+                }.await()
             }
         }
-        channel.shutdown()
     }
 
     @Test
     @DisplayName("Синхронное обращение к user service")
     fun testUserInfoSync() {
-        //создаем channel со свойствами по умолчанию
-        val channel = InvestApi.defaultChannel(
-            token = System.getProperty("token"),
-            target = System.getProperty("target")
-        )
-        //создаем объект API
-        val investApi = InvestApi.createApi(channel)
-        val usersService = investApi.usersServiceSync
         Assertions.assertDoesNotThrow {
-            // запрашиваем все аккаунты
-            val accounts = usersService.getAccounts(GetAccountsRequest.getDefaultInstance())
-            // выводим полученный результат в output
-            accounts.accountsList.forEach { logger.info("$it") }
+            syncExample.exampleUserInfo()
         }
-        channel.shutdown()
     }
 
     @Test
     @DisplayName("Получение ошибки unauthenticated")
     fun testUnauthenticated() {
-        //создаем channel с некорректным токеном
-        val channel = InvestApi.defaultChannel(
-            token = "invalid_token",
-            target = System.getProperty("target")
-        )
-        //создаем объект API
-        val investApi = InvestApi.createApi(channel)
-        val usersService = investApi.usersServiceSync
-        // запрашиваем аккаунты для получения ошибки
         Assertions.assertThrows(StatusException::class.java)
         {
-            usersService.getAccounts(GetAccountsRequest.getDefaultInstance())
+            syncExample.exampleUnauthenticatedAccess()
         }
-        channel.shutdown()
     }
 
 }
